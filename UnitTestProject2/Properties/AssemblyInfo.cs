c7391892ce29f7a687d0ae37a79fae08f932c406
212213 <212213@1305-005>
2025-02-17 16:21:04 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e25a53dd-e716-4141-9dac-1d20dea27b93")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject2/zadanie2.cs
@@ -0,0 +1,52 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using System;
+
+//namespace UnitTestProject2
+//{
+//    [TestClass]
+//    public class UnitTest1
+//    {
+//        static int IsSimple(int x)
+//        {
+//            bool simple = true;
+//            for (int i = 2; i <= x; i++)
+//                if (x % i == 0)
+//                {
+//                    simple = false;
+//                    break;
+//                }
+//            return simple;
+//        }
+//        [TestMethod]
+//        public void Simple17()
+//        {
+//            Assert.IsTrue(IsSimple(17));
+//        }
+
+
+//        [TestMethod]
+//        public void Simple72()
+//        {
+//            Assert.IsTrue(IsSimple(72));
+//        }
+
+
+//        [TestMethod]
+//        public void Simple1()
+//        {
+//            Assert.IsTrue(IsSimple(1));
+//        }
+
+//        [TestMethod]
+//        public void Simple0()
+//        {
+//            Assert.IsTrue(IsSimple(0));
+//        }
+
+//        [TestMethod]
+//        public void SimpleNegative()
+//        {
+//            Assert.IsTrue(IsSimple(-5));
+//        }
+//    }
+//}
